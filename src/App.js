e1a2d8efd13fa79e9177ac5803f204bf180e27ee
gauri9359060 <gaurip388@gmail.com>
2022-02-11 10:18:25 +0530

    mobileOS-react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Cards} from './components/Cards'
+
+function App() {
+  return (
+    <div className="App">
+      <div> 
+          <Cards />
+      </div>
+        
+    </div>
+  );
+}
+
+export default App;
